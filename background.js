109ca432101990f947b615d2d9a9df37b677107c
johnidol <giovanni.idili@gmail.com>
2016-08-13 12:29:12 +0100

    differntiate between initial  delay and loop delay

--- background.js
+++ background.js
@@ -4,7 +4,14 @@ $(function() {
 		console.log('hiding promoted tweets!');
 	}
 	
-	setInterval(hidePromoted, 3000);
+	var initialDelay = 1000;
+	var loopDelay = 3000; 
+	
+	// run once with a delay then run 
+	setTimeout(function(){ 
+		hidePromoted();
+		setInterval(hidePromoted, loopDelay);
+	}, initialDelay);
 	
 	console.log('promote my ass!');
 });
